c534356844425b7e3e09d747cb300413aa2c5be4
Venus Yan <venus.qm.yan@gmail.com>
2025-04-29 16:02:25 -0400

    Delete midpoint_rule.py

--- midpoint_rule.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def midpoint_rule(f, a, b, n):
-    h = (b - a) / n
-    total = 0
-    for i in range(n):
-        xi = a + h * (i + 0.5)
-        total += f(xi)
-    return h * total
