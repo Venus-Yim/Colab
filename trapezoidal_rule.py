12e04590e734b7bff8308e3bae35a2458030b2ce
Venus Yan <venus.qm.yan@gmail.com>
2025-04-29 16:02:52 -0400

    Delete trapezoidal_rule.py

--- trapezoidal_rule.py
+++
@@ -1,7 +0,0 @@
-def trapezoidal_rule(f, a, b, n):
-    h = (b - a) / n
-    total = 0.5 * (f(a) + f(b))
-    for i in range(1, n):
-        xi = a + i * h
-        total += f(xi)
-    return h * total
