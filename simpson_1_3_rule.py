c736d341e0cdeb895fbd9f7eca2e8b01229f06b9
Venus Yan <venus.qm.yan@gmail.com>
2025-05-01 13:33:10 -0400

    Update simpson_1_3_rule.py

--- simpson_1_3_rule.py
+++ simpson_1_3_rule.py
@@ -5,7 +5,7 @@ import math
 import csv
 
 def f(x):
-    return 10*np.sqrt(1 - (x**2) / 25)
+    return 10 * np.sin(np.pi / 5 * x) * np.pi
 
 def simpson_1_3_rule(f, a, b, n):
     if n % 2 != 0:
@@ -30,7 +30,7 @@ def main(n_samples):
     elapsed_time = (end_time - start_time).total_seconds()
     size_of_float = np.dtype(np.float64).itemsize
     memory_required = 3 * n_samples * size_of_float / (1024**3)
-    error = abs(area-12.5*math.pi)/(12.5*math.pi)
+    error = abs(area-100)/(100)
     write_to_csv([n_samples, area, memory_required, elapsed_time, error])
 
 if __name__ == '__main__':
